979c1cd7fe5a15fa434af84b867351068bfa0ae2
Harley Engen <152737849+plantpirate@users.noreply.github.com>
2025-02-15 20:34:57 -0500

    update add to library function

--- odin-book-library/script.js
+++ odin-book-library/script.js
@@ -11,6 +11,7 @@ function Book(title, author, pages, read) {
   };
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
 }
