c49802558a55a8fb8722f6aff1e958801fe5c77e
Harley Engen <152737849+plantpirate@users.noreply.github.com>
2025-02-15 20:17:36 -0500

    update book constructor

--- odin-book-library/script.js
+++ odin-book-library/script.js
@@ -1,7 +1,14 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read ? "have read" : "not read yet";
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages,          
+    ${this.read}`;
+  };
 }
 
 function addBookToLibrary() {
