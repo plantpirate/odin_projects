fb0663d7f657eeb105a83117a09ef47a390c7f1c
Harley Engen <152737849+plantpirate@users.noreply.github.com>
2025-04-05 15:17:47 -0400

    include for loops for board creation

--- odin-tic-tac-toe/script.js
+++ odin-tic-tac-toe/script.js
@@ -1,15 +1,24 @@
-const ticTacToe = (function () {
-  let gameboard = [];
+// const ticTacToe = (function () {
+//   let gameboard = [];
 
-  const createPlayer = (name) => {
-    const playerName = name;
+//   const createPlayer = (name) => {
+//     const playerName = name;
 
-    let score = 0;
-  };
-})();
+//     let score = 0;
+//   };
+// })();
 
 function GameBoard() {
   const rows = 3;
   const columns = 3;
   const board = [];
+
+  for (let i = 9; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
 }
