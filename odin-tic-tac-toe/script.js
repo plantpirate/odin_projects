572729c94f1facd810d76cc610c0b20b29c3087a
Harley Engen <152737849+plantpirate@users.noreply.github.com>
2025-04-06 19:52:30 -0400

    update player funcs placement into playerControl func

--- odin-tic-tac-toe/script.js
+++ odin-tic-tac-toe/script.js
@@ -22,20 +22,46 @@
 
 //   const getBoard = () => board;
 // }
-const Player = (name, mark) => {
-  const getName = () => name;
-  const getMark = () => mark;
-  return { getName, getMark };
-};
 
-const playerOne = ("Player One", "X");
-const playerTwo = ("Player Two", "O");
+// const Player = (name, mark) => {
+//   const getName = () => name;
+//   const getMark = () => mark;
+//   return { getName, getMark };
+// };
 
-let currentPlayer = "X";
+// const playerOne = ("Player One", "X");
+// const playerTwo = ("Player Two", "O");
 
-const switchPlayer = () => {
-  currentPlayer = currentPlayer === "X" ? "O" : "X";
-  console.log(currentPlayer);
+// let currentPlayer = "X";
+
+// const switchPlayer = () => {
+//   currentPlayer = currentPlayer === "X" ? "O" : "X";
+//   console.log(currentPlayer);
+// };
+
+const playerControl = (
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) => {
+  const players = [
+    {
+      name: playerOneName,
+      mark: "X",
+    },
+    {
+      name: playerTwoName,
+      mark: "O",
+    },
+  ];
+
+  let currentPlayer = players[0];
+
+  const getMark = () => currentPlayer.mark;
+  const getCurrentPlayer = () => currentPlayer;
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+  };
+  return { getCurrentPlayer, switchPlayer, getMark };
 };
 
 const GameBoard = () => {
@@ -51,3 +77,8 @@ const GameBoard = () => {
 };
 
 GameBoard();
+const playerController = playerControl();
+// console.log(playerController.getCurrentPlayer());
+console.log(playerController.getCurrentPlayer());
+playerController.switchPlayer();
+console.log(playerController.getCurrentPlayer());
